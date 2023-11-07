43805a8f23e8c7498950ba2f93cca791b0d3f500
Mohammad Noman <128275910+MohammadNoman054@users.noreply.github.com>
2023-11-07 22:56:17 +0500

    Created Restful API
    Created Restful API Using HTML, CSS and JQUERY

---
+++ script.js
@@ -0,0 +1,60 @@
+$(function () {
+
+  var $informations = $("#information");
+
+  $.ajax({
+    type: "GET",
+    url: "https://jsonplaceholder.typicode.com/todos/1",
+    success: function (informations) {
+      $informations.append("<li> UserID: " + informations.UserId + "  ID: " + informations.id + "  Title: " + informations.title + " completed: " + informations.completed + "  <button id='del'>Delete</button> </li>");
+    },
+    error: function () {
+      informations.innerHTML("Error! Can't get Data From API.")
+    }
+  });
+
+  $("#button").on("click", function () {
+    var information = {
+      UserId: $("#UserId").val(),
+      id: $("#id").val(),
+      title: $("#title").val(),
+      completed: $("#completed").val()
+    };
+    
+
+    $.ajax({
+      type: "POST",
+      url: "https://jsonplaceholder.typicode.com/todos/",
+      data: information,
+      success: function (newInformation) {
+        $informations.append("<li> UserID: " + newInformation.UserId + "  ID: " + newInformation.id + "  Title: " + newInformation.title + " completed: " + newInformation.completed + "  <button id ='del'>Delete</button>"+"</li>");
+      },
+      error: function () {
+        alert("ERROR!")
+      }
+    });
+  });
+
+
+  $("#del").on("click", function () {
+    var $li = $(this).closest("li"); 
+
+    $.ajax({
+      type: "DELETE",
+      url: "https://jsonplaceholder.typicode.com/todos/",
+      success: function () {
+        $li.remove();
+      },
+      error: function () {
+        alert("ERROR!")
+      }
+    });
+  });
+
+});
+
+
+
+
+
+
